fd15ebd4af7cffd6644a73ea3dd252d81383a3ca
Brareyesb15 <116505799+Brareyesb15@users.noreply.github.com>
2022-12-27 11:47:22 -0500

    estados

--- 08-React-Estado-LifeCycle/homework/01 - Exercises/src/components/Animals/Animals.jsx
+++ 08-React-Estado-LifeCycle/homework/01 - Exercises/src/components/Animals/Animals.jsx
@@ -6,6 +6,17 @@ export default class Animals extends React.Component {
   }
 
   render () {
-    return <div></div>
+    return <div>
+      {this.props.animals.map((a,i)=> 
+      <div key={i}>   
+      <h5>{a.name}</h5>
+      <img src={a.image} alt= {a.name} width = "300px"></img>
+      <span>{a.specie}</span>
+      </div>)
+      }
+
+
+    
+    </div>
   }
 }
--- 08-React-Estado-LifeCycle/homework/01 - Exercises/src/components/Species/Species.jsx
+++ 08-React-Estado-LifeCycle/homework/01 - Exercises/src/components/Species/Species.jsx
@@ -1,8 +1,11 @@
 import React from 'react'
 
-export default function Species () {
+export default function Species (props) {
   return (
-    <div>
+    <div> 
+      <h2 >Species</h2>
+      {props.species.map((a,i) => (<button key = {i} onClick={props.handleSpecies} value = {a}>{a}</button> ))}
+      <button onClick={props.handleAllSpecies}>All Animals</button> 
 
     </div>
   )
